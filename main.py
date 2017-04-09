a1195b681c5a327f96d8276b2a20d162988c7975
Evgeny <krivosheevevgeny@gmail.com>
2017-04-09 16:57:49 +0200

    refactoring/main file

--- main.py
+++ main.py
@@ -2,10 +2,40 @@
 run experiments
 '''
 
-from cf_simulation_synthetic import synthesizer
 import numpy as np
+from cf_simulation_synthetic import synthesizer
+from quiz_simulation import do_quiz
 
-if __name__ == '__main__':
+
+def run_quiz():
+    statistic_passed = {
+        'rand_ch': 0,
+        'smart_ch': 0,
+        'worker': 0
+    }
+    statistic_total = {
+        'rand_ch': 0,
+        'smart_ch': 0,
+        'worker': 0
+    }
+    for _ in range(1000):
+        result = do_quiz(trust_min=0.75, quiz_papers_n=4, cheaters_prop=0.5)
+        if len(result) > 1:
+            statistic_passed[result[2]] += 1
+            statistic_total[result[2]] += 1
+        else:
+            statistic_total[result[0]] += 1
+
+    rand_cheaters_passed = statistic_passed['rand_ch'] / float(statistic_total['rand_ch']) * 100
+    smart_cheaters_passed = statistic_passed['smart_ch'] / float(statistic_total['smart_ch']) * 100
+    workers_passed = statistic_passed['worker'] / float(statistic_total['worker']) * 100
+
+    print 'random cheaters passed: {}%'.format(rand_cheaters_passed)
+    print 'smart cheaters passed: {}%'.format(smart_cheaters_passed)
+    print 'workers passed: {}%'.format(workers_passed)
+
+
+def run_task():
     tests_page_params = [1, 1, 1, 2, 2, 3]
     papers_page_params = [1, 2, 3, 2, 3, 3]
     for test_page, papaers_page in zip(tests_page_params, papers_page_params):
@@ -13,8 +43,10 @@ if __name__ == '__main__':
         budget_spent_list = []
         for _ in range(1000):
             job_accuracy, budget_spent, paid_pages_n = synthesizer(trust_min=1., n_criteria=3,
-                        test_page=test_page, papers_page=papaers_page, quiz_papers_n=4, n_papers=18, budget=50,
-                        price_row=0.4, judgment_min=3, judgment_max=5, cheaters_prop=0.1)
+                                                                   test_page=test_page, papers_page=papaers_page,
+                                                                   quiz_papers_n=4, n_papers=18, budget=50,
+                                                                   price_row=0.4, judgment_min=3, judgment_max=5,
+                                                                   cheaters_prop=0.1)
             job_accuracy_list.append(job_accuracy)
             budget_spent_list.append(budget_spent)
 
@@ -31,3 +63,7 @@ if __name__ == '__main__':
               'job_accuracy_std={}\n' \
               'budget_spent_avg={}$\n' \
               'budget_spent_std={}$\n'.format(job_accuracy_avg, job_accuracy_std, budget_spent_avg, budget_spent_std)
+
+
+if __name__ == '__main__':
+    run_quiz()
--- quiz_simulation.py
+++ quiz_simulation.py
@@ -40,30 +40,3 @@ def do_quiz(trust_min, quiz_papers_n, cheaters_prop):
         return (worker_trust, worker_accuracy, worker_type)
     return (worker_type,)
 
-
-if __name__ == '__main__':
-    statistic_passed = {
-        'rand_ch': 0,
-        'smart_ch': 0,
-        'worker': 0
-    }
-    statistic_total = {
-        'rand_ch': 0,
-        'smart_ch': 0,
-        'worker': 0
-    }
-    for _ in range(1000):
-        result = do_quiz(trust_min=0.75, quiz_papers_n=4, cheaters_prop=0.5)
-        if len(result) > 1:
-            statistic_passed[result[2]] += 1
-            statistic_total[result[2]] += 1
-        else:
-            statistic_total[result[0]] += 1
-
-    rand_cheaters_passed = statistic_passed['rand_ch']/float(statistic_total['rand_ch'])*100
-    smart_cheaters_passed = statistic_passed['smart_ch']/float(statistic_total['smart_ch'])*100
-    workers_passed = statistic_passed['worker']/float(statistic_total['worker'])*100
-
-    print 'random cheaters passed: {}%'.format(rand_cheaters_passed)
-    print 'smart cheaters passed: {}%'.format(smart_cheaters_passed)
-    print 'workers passed: {}%'.format(workers_passed)
