18dce40282a632315c2fcb3285e5ea58a9887cc2
Evgeny <krivosheevevgeny@gmail.com>
2017-04-10 14:42:06 +0200

    visualisation/new data for R-script

--- visualisation/quiz.R
+++ visualisation/quiz.R
@@ -1,7 +1,28 @@
-data <- matrix(c(31.19, 84.29, 92.01, 
-                 6.28, 49.38, 70.66, 
-                 11.07, 82.80, 90.57,
-                 1.56, 47.97, 69.70),
+# IN/OUT
+# (trust_min=0.75, quiz_papers_n=4, cheaters_prop=0.5)
+# random cheaters passed: 31.1301347933%
+# smart cheaters passed: 65.1398759233%
+# workers passed: 83.0384528428%
+# 
+# (trust_min=1., quiz_papers_n=4, cheaters_prop=0.5)
+# random cheaters passed: 6.23740362776%
+# smart cheaters passed: 24.0860810047%
+# workers passed: 53.1543248724%
+# 
+# (trust_min=0.75, quiz_papers_n=6, cheaters_prop=0.5)
+# random cheaters passed: 10.9850994038%
+# smart cheaters passed: 42.057206336%
+# workers passed: 71.0914867957%
+# 
+# (trust_min=1., quiz_papers_n=6, cheaters_prop=0.5)
+# random cheaters passed: 1.56520697777%
+# smart cheaters passed: 11.7677029049%
+# workers passed: 42.8005109721%
+
+data <- matrix(c(31.13, 65.40, 83.04, 
+                 6.24, 24.09, 53.15, 
+                 10.10, 42.06, 71.10,
+                 1.57, 11.77, 42.80),
                ncol=3,byrow=TRUE)
 colnames(data) <- c("Rd_cheaters","Smart_cheaters","Workers")
 rownames(data) <- c("trsh: 75%\npapers: 4",
