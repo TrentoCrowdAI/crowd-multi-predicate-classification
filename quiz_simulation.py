42d6de73cb0e4b4a05c46b2b659e722b27403b20
Evgeny <krivosheevevgeny@gmail.com>
2017-06-06 09:41:11 +0200

    fix/quiz simulation/cheaters acc

--- quiz_simulation.py
+++ quiz_simulation.py
@@ -62,8 +62,7 @@ def do_quiz_criteria_confm(quiz_papers_n, cheaters_prop):
     # decide if a worker a cheater
     if np.random.binomial(1, cheaters_prop):
         worker_type = 'rand_ch'
-        worker_accuracy_in = 0.5
-        worker_accuracy_out = 0.5
+        worker_accuracy_out, worker_accuracy_in = 0.5, 0.5
     else:
         worker_type = 'worker'
         worker_accuracy_in = 0.5 + (np.random.beta(1, 1) * 0.5)
@@ -80,10 +79,16 @@ def do_quiz_criteria_confm(quiz_papers_n, cheaters_prop):
         # 50% IN papers
         if np.random.binomial(1, 0.5):
             for mult in [1., 1., 1.1, 0.9]:
-                if not np.random.binomial(1, worker_accuracy_in*mult if worker_accuracy_in*mult <= 1. else 1.):
+                if worker_type == 'rand_ch':
+                    if not np.random.binomial(1, worker_accuracy_in):
+                        return [worker_type]
+                elif not np.random.binomial(1, worker_accuracy_in*mult if worker_accuracy_in*mult <= 1. else 1.):
                     return [worker_type]
         else:
             for mult in [1., 1., 1.1, 0.9]:
-                if not np.random.binomial(1, worker_accuracy_out*mult if worker_accuracy_out*mult <= 1. else 1.):
+                if worker_type == 'rand_ch':
+                    if not np.random.binomial(1, worker_accuracy_out):
+                        return [worker_type]
+                elif not np.random.binomial(1, worker_accuracy_out*mult if worker_accuracy_out*mult <= 1. else 1.):
                     return [worker_type]
     return [worker_accuracy_out, worker_accuracy_in, worker_type]
