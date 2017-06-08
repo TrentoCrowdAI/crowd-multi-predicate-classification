77e91b2ccbf2e3b9af22d084b600459b4cc19ddd
Evgeny <krivosheevevgeny@gmail.com>
2017-06-08 11:29:21 +0200

    feature/aggregate_papers & estimate loss

--- criteria_based_simulation.py
+++ criteria_based_simulation.py
@@ -1,6 +1,6 @@
 from generator import generate_responses_gt
 from fusion_algorithms.dawid_skene import dawid_skene
-from utils import run_quiz_criteria_confm, classify_papers
+from utils import run_quiz_criteria_confm, aggregate_papers, estimate_loss
 
 if __name__ == '__main__':
     z = 0.3
@@ -15,7 +15,5 @@ if __name__ == '__main__':
         for J in [2, 3, 5, 10]:
             responses, GT = generate_responses_gt(n_papers, criteria_power, papers_page, J, acc, criteria_difficulty)
             values_prob = dawid_skene(responses, tol=0.00001, max_iter=10)  # TO DO max_iter=100
-            classified_papers = classify_papers(n_papers, criteria_num, values_prob, cost)
-            # - post processing
-            # - get metrics
-            pass
+            papers_prob_in = aggregate_papers(n_papers, criteria_num, values_prob)
+            loss = estimate_loss(papers_prob_in, cost)
--- utils.py
+++ utils.py
@@ -11,13 +11,54 @@ def run_quiz_criteria_confm(quiz_papers_n, cheaters_prop, criteria_difficulty):
     return acc_passed_distr
 
 
-def classify_papers(n_papers, criteria_num, values_prob, cost):
-    classified_papers = []
-    exclusion_trsh = cost / (cost + 1.)
+def aggregate_papers(n_papers, criteria_num, values_prob):
+    papers_prob_in = []
     for paper_id in range(n_papers):
-        p_exclusion = 1.
+        prob_in = 1.
         for e_paper_id in range(criteria_num):
-            p_exclusion *= 1 - values_prob[paper_id*criteria_num+e_paper_id][1]
-        p_exclusion = 1 - p_exclusion
-        classified_papers.append(1) if p_exclusion > exclusion_trsh else classified_papers.append(0)
-    return classified_papers
+            prob_in *= 1 - values_prob[paper_id*criteria_num+e_paper_id][1]
+        papers_prob_in.append(prob_in)
+    return papers_prob_in
+
+
+def estimate_loss(papers_prob_in, cost):
+    inclusion_trsh = 1 - cost / (cost + 1.)
+    loss_total = 0.
+    for p_in in papers_prob_in:
+        if p_in > inclusion_trsh:
+            loss_total += 1 - p_in
+        else:
+            loss_total += cost * p_in
+    loss_per_paper = loss_total / len(papers_prob_in)
+    return loss_per_paper
+
+
+# def get_loss(classified_papers, GT, cost, criteria_num):
+#     # obtain GT scope values for papers
+#     GT_scope = []
+#     for paper_id in range(len(classified_papers)):
+#         if sum([GT[paper_id * criteria_num + e_paper_id] for e_paper_id in range(criteria_num)]):
+#             GT_scope.append(0)
+#         else:
+#             GT_scope.append(1)
+#     i_gt = sum(GT_scope)
+#     e_gt = len(GT_scope) - i_gt
+#     fe_num = 0.
+#     fi_num = 0.
+#     for cl_val, gt_val in zip(classified_papers, GT_scope):
+#         if gt_val and not cl_val:
+#             fe_num += 1
+#         if not gt_val and cl_val:
+#             fi_num += 1
+#     loss = fe_num / i_gt * cost + fi_num / e
+
+# def classify_papers(n_papers, criteria_num, values_prob, cost):
+#     classified_papers = []
+#     exclusion_trsh = cost / (cost + 1.)
+#     for paper_id in range(n_papers):
+#         p_exclusion = 1.
+#         for e_paper_id in range(criteria_num):
+#             p_exclusion *= 1 - values_prob[paper_id*criteria_num+e_paper_id][1]
+#         p_exclusion = 1 - p_exclusion
+#         classified_papers.append(1) if p_exclusion > exclusion_trsh else classified_papers.append(0)
+#     return classified_papers
