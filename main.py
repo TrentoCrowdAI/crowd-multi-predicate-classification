c19ed737788fa6de633a118653fc6237aee8f613
Evgeny <krivosheevevgeny@gmail.com>
2017-07-20 21:53:43 +0300

    feature/baseline_dawid output

--- main.py
+++ main.py
@@ -23,6 +23,9 @@ if __name__ == '__main__':
             cost_baseline = (Nt + papers_page * criteria_num) * J / float(papers_page)
             loss_baseline_list = []
             fp_b, tp_b, rec_b, pre_b = [], [], [], []
+
+            loss_baseline_list_dawid = []
+            fp_d, tp_d, rec_d, pre_d = [], [], [], []
             for _ in range(10):
                 # quiz, generation responses
                 acc = run_quiz_criteria_confm(Nt, z, [1.])
@@ -38,16 +41,29 @@ if __name__ == '__main__':
                 pre_b.append(pre_b_)
 
                 # baseline dawid
-                baseline_dawid(responses, criteria_num, n_papers, GT, cr)
-            print 'BASELINE  loss: {:1.2f}, price: {:1.2f}, fp_rate: {:1.2f}, tp_rate: {:1.2f}, ' \
+                loss_baseline_d, fp_rate_d, tp_rate_d, rec_d_, pre_d_ = baseline_dawid(responses,
+                                                                      criteria_num, n_papers, GT, cr)
+                loss_baseline_list_dawid.append(loss_baseline_d)
+                fp_d.append(fp_rate_d)
+                tp_d.append(tp_rate_d)
+                rec_d.append(rec_d_)
+                pre_d.append(pre_d_)
+            print 'BASELINE DONG  loss: {:1.2f}, price: {:1.2f}, fp_rate: {:1.2f}, tp_rate: {:1.2f}, ' \
                   'recall: {:1.2f}, precision: {:1.2f}'.\
                 format(np.mean(loss_baseline_list), cost_baseline, np.mean(fp_b), np.mean(tp_b),
                        np.mean(rec_b), np.mean(pre_b))
+            print 'BASELINE DAWID loss: {:1.2f}, price: {:1.2f}, fp_rate: {:1.2f}, tp_rate: {:1.2f}, ' \
+                  'recall: {:1.2f}, precision: {:1.2f}'. \
+                format(np.mean(loss_baseline_list_dawid), cost_baseline, np.mean(fp_d), np.mean(tp_d),
+                       np.mean(rec_d), np.mean(pre_d))
             print '---------------------'
 
             data.append([Nt, J, cr, np.mean(loss_baseline_list), np.std(loss_baseline_list),
-                         np.mean(fp_b), np.mean(tp_b), cost_baseline, 0., 'Baseline',
+                         np.mean(fp_b), np.mean(tp_b), cost_baseline, 0., 'dong',
                          np.mean(rec_b), np.mean(pre_b)])
+            data.append([Nt, J, cr, np.mean(loss_baseline_list_dawid), np.std(loss_baseline_list_dawid),
+                         np.mean(fp_d), np.mean(tp_d), cost_baseline, 0., 'dawid',
+                         np.mean(rec_d), np.mean(pre_d)])
     pd.DataFrame(data, columns=['Nt', 'J', 'lr', 'loss_mean', 'loss_std', 'FPR', 'TPR',
                                 'price_mean', 'price_std', 'alg', 'recall', 'precision']). \
-                                to_csv('output/data/loss_tests_cr5.csv', index=False)
+                                to_csv('output/data/dong_dawid_mv.csv', index=False)
