6a8bfc5a51e3b7c4ab48a98cf851d7e0cb0646f4
Evgeny Krivosheev <K1r9i9v3osheev>
2018-02-15 19:06:38 +0100

    refactoring

--- real_data_experiment.py
+++ real_data_experiment.py
@@ -15,24 +15,33 @@ n_papers = 100
 
 def get_data():
     data = pd.read_csv('output/amt_data/crowd-data-corrected.csv')
-    # workers_ids_ch = set(list(pd.unique(data['intervention worker ID'])) \
-    #                                + list(pd.unique(data['use of tech worker ID'])) \
-    #                                + list(pd.unique(data['older adult worker ID'])))
-    workers_ids_ch = set(list(pd.unique(data['use of tech worker ID'])) \
-                         + list(pd.unique(data['older adult worker ID'])))
+
+    if n_criteria == 3:
+        workers_ids_ch = set(list(pd.unique(data['intervention worker ID'])) \
+                               + list(pd.unique(data['use of tech worker ID'])) \
+                               + list(pd.unique(data['older adult worker ID'])))
+        # property of the data file
+        criteria = {0: 'intervention Vote',
+                    1: 'use of tech vote',
+                    2: 'older adult vote'}
+        column_workers_ids = ['intervention worker ID', 'use of tech worker ID', 'older adult worker ID']
+        gold_columns = ['GOLD INTERVENTION', 'GOLD USE OF TECH', 'GOLD OLD']
+
+    if n_criteria == 2:
+        workers_ids_ch = set(list(pd.unique(data['use of tech worker ID'])) \
+                             + list(pd.unique(data['older adult worker ID'])))
+        # property of the data file
+        criteria = {
+                    0: 'use of tech vote',
+                    1: 'older adult vote'}
+        column_workers_ids = ['use of tech worker ID', 'older adult worker ID']
+        gold_columns = ['GOLD USE OF TECH', 'GOLD OLD']
+
+
     workers_ids_ch = [id_ch for id_ch in workers_ids_ch if type(id_ch) != float]
     workers_data = [[] for _ in workers_ids_ch]
 
     paper_ids_dict = dict(zip(set(data['paper ID']), range(n_papers)))
-    # property of the data file
-    # criteria = {0: 'intervention Vote',
-    #             1: 'use of tech vote',
-    #             2: 'older adult vote'}
-    # column_workers_ids = ['intervention worker ID', 'use of tech worker ID', 'older adult worker ID']
-    criteria = {
-                0: 'use of tech vote',
-                1: 'older adult vote'}
-    column_workers_ids = ['use of tech worker ID', 'older adult worker ID']
     for w_id, w_ch in enumerate(workers_ids_ch):
         for c_id, c_name in criteria.iteritems():
             column_workers = column_workers_ids[c_id]
@@ -50,8 +59,7 @@ def get_data():
     GT = [None]*n_papers*n_criteria
     for paper_id_raw in paper_ids_dict.keys():
         paper_id = paper_ids_dict[paper_id_raw]
-        # for cr_id, cr_gold_colmn in zip(range(n_criteria), ['GOLD INTERVENTION', 'GOLD USE OF TECH', 'GOLD OLD']):
-        for cr_id, cr_gold_colmn in zip(range(n_criteria), ['GOLD USE OF TECH', 'GOLD OLD']):
+        for cr_id, cr_gold_colmn in zip(range(n_criteria), gold_columns):
             gold_value_raw = data[data['paper ID'] == paper_id_raw][cr_gold_colmn].values[0]
             gold_value = 0 if gold_value_raw == 1 else 1
             GT[paper_id * n_criteria + cr_id] = gold_value
@@ -129,8 +137,8 @@ if __name__ == '__main__':
     fr_p_part = .5
     data = []
     votes_worker = 20.
-    for Nt in [3, 4, 5]:
-        for J in [3, 5, 10]:
+    for Nt in [2, 3, 4, 5]:
+        for J in [3, 5]:
             print 'Nt: {}, J: {}'.format(Nt, J)
             w_data, GT = get_data()
             w_data = do_quiz(w_data, GT, Nt)
@@ -145,7 +153,7 @@ if __name__ == '__main__':
             loss_m, rec_m, pre_m, f_m, price_m = [], [], [], [], []
             loss_sm, rec_sm, pre_sm, f_sm, price_sm, syn_prop_sm = [], [], [], [], [], []
 
-            for _ in range(30):
+            for _ in range(50):
                 cj_votes, counter = j_correction(c_votes, criteria_accuracy, GT, J)
                 syn_votes_prop = float(counter) / (len(c_votes) * J)
                 # Baseline
@@ -183,8 +191,8 @@ if __name__ == '__main__':
             print 'SM-RUN    syn_votes_prop: {}, loss: {:1.2f}, price: {:1.2f}, recall: {:1.2f}, precision: {:1.2f}, f_b: {}, f_b_std: {}'. \
                 format(np.mean(syn_prop_sm), np.mean(loss_sm), np.mean(price_sm), np.mean(rec_sm), np.mean(pre_sm), np.mean(f_sm), np.std(f_sm))
 
+            print 'std_pres: ', np.std(pre_sm), 'std_rec: ', np.std(rec_sm)
             print '---------------------'
-
             data.append([Nt, J, lr, np.mean(loss_b), np.std(loss_b), syn_votes_prop, np.mean(price_b), np.mean(rec_b),
                          np.mean(pre_b), np.mean(f_b), np.std(f_b), 'Baseline'])
             data.append([Nt, J, lr, np.mean(loss_m), np.std(loss_m), syn_votes_prop, np.mean(price_m), np.mean(rec_m),
@@ -193,4 +201,4 @@ if __name__ == '__main__':
                          np.mean(pre_sm), np.mean(f_sm), np.std(f_sm), 'SM-runs'])
     pd.DataFrame(data, columns=['Nt', 'J', 'lr', 'loss_mean', 'loss_std', 'syn_votes_prop',
                                 'price_mean', 'recall', 'precision', 'f_beta', 'f_beta_std', 'alg']). \
-        to_csv('output/data/experimental_results.csv', index=False)
+        to_csv('output/data/experimental_results_cr{}_stpXXX.csv'.format(n_criteria), index=False)
